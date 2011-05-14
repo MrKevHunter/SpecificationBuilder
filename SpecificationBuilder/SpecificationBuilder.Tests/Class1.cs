f4baccbb41d306ea9a6960f9fde84ddb027bb60e
Kev Hunter <MrKevHunter@gmail.com>
2011-05-14 05:46:03 +0100

    init

---
+++ SpecificationBuilder/SpecificationBuilder.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecificationBuilder.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ SpecificationBuilder/SpecificationBuilder/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecificationBuilder
+{
+    public class Class1
+    {
+    }
+}
